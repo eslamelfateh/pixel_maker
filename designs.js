05e0edb64b2b8c0343448b5eea2c644af5fcbc5a
EslamElfateh <eslam.elfateh.94@gmail.com>
2019-10-09 15:54:57 +0200

    Add DOM selectors

--- designs.js
+++ designs.js
@@ -1,5 +1,16 @@
-// Select color input
+// Function DOM SELECTOR
+let select = function (x) {
+  return document.querySelector(x);
+};
+
 // Select size input
+const row = select("#inputHeight");
+const col = select("#inputWidth");
+
+// Select color input
+const colorPicker = select("#colorPicker");
+const pixelCanvas = select("#pixelCanvas");
+
 
 // When size is submitted by the user, call makeGrid()
 
